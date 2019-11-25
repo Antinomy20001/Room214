9ba9a094797ab7bab184a0d099d953f91f6a803e
Antinomy20001 <ann1026766512@outlook.com>
2019-11-25 20:15:12 +0800

    fix

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-supervisord -n -c /app/deploy/supervisord.conf
+gunicorn access_check.wsgi --bind 127.0.0.1:8080 --workers 5 --threads 4 --max-requests-jitter 10000 --max-requests 1000000 --keep-alive 32
